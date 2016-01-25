5a4ed8247e3e2fc16ef50b6d5b3750730e4a561f
Alan <nhnam.develop@gmail.com>
2016-01-26 00:07:27 +0800

    [+] Add Waiting room. This room is used by requester, who will be waiting for data response, the same reason of respone, the same seat they on.

--- NNClientService/Core/NNClientWaitingRoom.swift
+++ NNClientService/Core/NNClientWaitingRoom.swift
@@ -8,6 +8,32 @@
 
 import UIKit
 
-class NNClientWaitingRoom: NSObject {
+class NNClientWaitingRoom:NSObject {
 
+    class var sharedInstance:NNClientWaitingRoom {
+        struct Static {
+            static var onceToken: dispatch_once_t = 0
+            static var instance: NNClientWaitingRoom? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = NNClientWaitingRoom()
+        }
+        return Static.instance!
+    }
+    
+    private override init() {
+        debugPrint("Init NNClientWaitingRoom sharedInstance")
+    }
+    
+    deinit {
+        
+    }
+    
+    func seatAndWait(path:String, params:[String: AnyObject]!) -> Void {
+        
+    }
+    
+    func getDataAndGoOut(path:String, params:[String: AnyObject]!, data:AnyObject!) ->Void {
+        
+    }
 }
