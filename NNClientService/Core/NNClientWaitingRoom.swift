f9d6d9d8e46e769348bc656762d6fa33b6b0a042
Alan <nhnam.develop@gmail.com>
2016-01-26 00:07:19 +0800

    [+] Add Waiting room. This room is used by requester, who will be waiting for data response, the same reason of respone, the same seat they on.

---
+++ NNClientService/Core/NNClientWaitingRoom.swift
@@ -0,0 +1,13 @@
+//
+//  NNClientWaitingRoom.swift
+//  NNClientService
+//
+//  Created by Nguyen Hoang Nam on 26/1/16.
+//  Copyright © 2016 Nguyen Hoang Nam. All rights reserved.
+//
+
+import UIKit
+
+class NNClientWaitingRoom: NSObject {
+
+}
