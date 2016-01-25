b2d0ee5a14fcd664ee882d59d87122f028673577
Alan <nhnam.develop@gmail.com>
2016-01-25 23:58:31 +0800

    [+] Add DataResponseProtocol

---
+++ NNClientService/Core/NNClientDataResponseProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  NNClientDataResponseProtocol.swift
+//  NNClientService
+//
+//  Created by Nguyen Hoang Nam on 25/1/16.
+//  Copyright © 2016 Nguyen Hoang Nam. All rights reserved.
+//
+
+import UIKit
+
+class NNClientDataResponseProtocol: NSObjectProtocol {
+
+}
